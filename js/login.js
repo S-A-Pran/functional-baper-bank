b85b136ae224dbb7ea9ace87225bffc7600f4c65
ingeniousSabbir <msabbir081@gmail.com>
2021-08-18 23:26:36 +0600

    bug fixed

--- js/login.js
+++ js/login.js
@@ -2,7 +2,7 @@ document.getElementById('login-submit').addEventListener('click', function(event
     const userMail = document.getElementById("user-email").value;
     const password = document.getElementById("user-password").value;
     if(userMail == 'user' && password == '12345'){
-        window.location.href = '../banking.html';
+        window.location.href = 'https://s-a-pran.github.io/functional-baper-bank/banking.html';
     }
     else{
         alert('wrong user or password');
