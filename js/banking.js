e3431bb78f04b3629a367c0e0c4df007b572405d
ingeniousSabbir <msabbir081@gmail.com>
2021-08-19 00:31:11 +0600

    bug fixed

--- js/banking.js
+++ js/banking.js
@@ -1,39 +1,57 @@
-function updateValue(currentFieldId,inputFieldId){
+function updateValue(currentFieldId,inputFieldId,isTrue){
+    const currentBalance = document.getElementById("account-balance").innerText;
+    const currentBalanceAmount = parseFloat(currentBalance);
     const previousTotal = document.getElementById(currentFieldId).innerText;
     const newInput = document.getElementById(inputFieldId).value;
-    const updateTotal = parseFloat(previousTotal) + parseFloat(newInput);
-    document.getElementById(currentFieldId).innerText = updateTotal;
-    //clearing input field
-    
+    if(isTrue == true){
+        if(parseFloat(newInput)>0){
+            const updateTotal = parseFloat(previousTotal) + parseFloat(newInput);
+            document.getElementById(currentFieldId).innerText = updateTotal;
+        }
+        else{
+            alert('Wrong Input');
+        }
+    }
+    else{
+        if(currentBalanceAmount > parseFloat(newInput) && parseFloat(newInput)>0){
+            const updateTotal = parseFloat(previousTotal) + parseFloat(newInput);
+            document.getElementById(currentFieldId).innerText = updateTotal;  
+        }
+        else{
+            alert('Wrong Input');
+        }
+    }
 }
 
 function balanceUpdate(currentBalance,inputFieldId,isTrue){
     //debugger;
-    const newDepositInput = document.getElementById(inputFieldId).value;
+    const newInput = document.getElementById(inputFieldId).value;
     if(isTrue == true){
-        updateBalance = parseFloat(currentBalance) + parseFloat(newDepositInput);
-        document.getElementById('account-balance').innerText = updateBalance;
+        if(parseFloat(newInput)>0){
+            updateBalance = parseFloat(currentBalance) + parseFloat(newInput);
+            document.getElementById('account-balance').innerText = updateBalance;
+        }
     }
     else{
-        updateBalance = parseFloat(currentBalance) - parseFloat(newDepositInput);
-        document.getElementById('account-balance').innerText = updateBalance;
+        if(parseFloat(newInput)<parseFloat(currentBalance) && parseFloat(newInput)>0){
+            updateBalance = parseFloat(currentBalance) - parseFloat(newInput);
+            document.getElementById('account-balance').innerText = updateBalance;
+        }
     }
     document.getElementById(inputFieldId).value = '';
 }
 
 //deposit btn field data handling
 document.getElementById("deposit-btn").addEventListener('click',function(event){
-    updateValue('deposit-total','deposit-input');
+    updateValue('deposit-total','deposit-input',true);
     //updating Balance
     const currentBalance = document.getElementById('account-balance').innerText;
     balanceUpdate(currentBalance,'deposit-input',true);
 });
 
-
 //withdraw btn handler
-
 document.getElementById("withdraw-btn").addEventListener('click', function(event){
-    updateValue('withdraw-amount','withdraw-input');
+    updateValue('withdraw-amount','withdraw-input',false);
     //updating Balance
     const currentBalance = document.getElementById('account-balance').innerText;
     balanceUpdate(currentBalance,'withdraw-input',false);
